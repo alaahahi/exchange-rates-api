7960f3b17c72ea973b97866d61d30a09cb51bdea
alaahahi <alaa.hahe@gmail.com>
2026-08-03 16:37:23 +0300

    شي

--- config/cors.php
+++ config/cors.php
@@ -1,17 +1,21 @@
 <?php
 
-$frontendUrl = env('FRONTEND_URL', 'http://localhost:4321');
+$frontendUrls = array_values(array_filter(array_map(
+    'trim',
+    explode(',', (string) env('FRONTEND_URL', 'http://localhost:4321,https://dinar-now.com')),
+)));
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_values(array_filter([
-        $frontendUrl,
-    ])),
+    'allowed_origins' => $frontendUrls,
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://([a-z0-9-]+\.)?dinar-now\.com$#i',
+        '#^https://[a-z0-9-]+\.workers\.dev$#i',
+    ],
 
     'allowed_headers' => ['*'],
 
